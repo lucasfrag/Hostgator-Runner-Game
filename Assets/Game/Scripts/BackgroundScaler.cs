5dfc47ec51bbb8e69e1a824e05e368edbcd671b0
Lucas Fraga <ti.lucasfraga@gmail.com>
2020-07-06 01:45:46 -0300

    Criação de personagem, background e movimentação dos objetos

---
+++ Assets/Game/Scripts/BackgroundScaler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScaler : MonoBehaviour
+{
+    private Vector3 startScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startScale = transform.localScale;
+
+        float height = Camera.main.orthographicSize * 2;
+        float width = height * Screen.width / Screen.height;
+
+        transform.localScale = new Vector3(width, height, startScale.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Game/Scripts/BackgroundTextureAnimator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundTextureAnimator : MonoBehaviour
+{
+    private Material mat;
+    private Vector2 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = gameObject.GetComponent<Renderer>().material;
+        offset = mat.GetTextureOffset("_MainTex");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset.x = offset.x + (0.2f * Time.deltaTime);
+        mat.SetTextureOffset("_MainTex", offset);
+    }
+}
